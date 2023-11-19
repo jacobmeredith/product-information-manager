7933cd06ec2bb5a1cb11f88bc9c9ccfdace1e307
Jacobmeredith <jacobmeredith@hotmail.co.uk>
2023-11-19 22:37:15 +0000

    add account tables

---
+++ api/database/migrations/000002_create_account_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS account(
+  id text PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL UNIQUE,
+);
+
+CREATE TABLE IF NOT EXISTS account_user(
+  role TEXT NOT NULL,
+  account_id TEXT NOT NULL,
+  user_id TEXT NOT NULL,
+  PRIMARY KEY (account_id, user_id),
+  FOREIGN KEY (account_id)
+    REFERENCES account(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (user_id)
+    REFERENCES user(id)
+    ON DELETE CASCADE
+);
