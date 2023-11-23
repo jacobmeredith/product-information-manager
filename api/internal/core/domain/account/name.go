221abd9b1ac7bf083bfc9f7758b40b87b6200ac9
Jacobmeredith <jacobmeredith@hotmail.co.uk>
2023-11-23 22:34:33 +0000

    add length limit to name

--- api/internal/core/domain/account/name.go
+++ api/internal/core/domain/account/name.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	ErrEmptyName    = errors.New("empty name")
+	ErrNameLength   = errors.New("name should be 4 or more characters")
 	ErrSpecialChars = errors.New("name should not contain any special characters")
 )
 
@@ -15,6 +16,10 @@ type Name string
 func NewName(name string) (Name, error) {
 	if name == "" {
 		return "", ErrEmptyName
+	}
+
+	if len(name) < 4 {
+		return "", ErrNameLength
 	}
 
 	if strings.ContainsAny(name, "!@#$%^&*()_+{}|:<>?") {
